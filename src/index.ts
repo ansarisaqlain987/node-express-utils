8ca6b188da6f104d5b290e0922f2cd808107345c
Saqlain Ansari <saqi@Saqlains-Mac-Studio.local>
2024-01-22 20:16:13 +0530

    added build config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-import { ExpressNextFunction, ExpressRequest, ExpressResponse } from "./types";
-import { encryptData, initJWT, verifyToken, decryptData } from "./utils/jwt-util";
-import { logger as customLogger } from "./utils/request-util";
-
-export type Request = ExpressRequest;
-export type Response = ExpressResponse;
-export type NextFunction = ExpressNextFunction;
-
-export const jwt = initJWT;
-export const verifyJwtToken = verifyToken;
-export const encryptString = encryptData;
-export const decryptString = decryptData;
-export const logger = customLogger;
+export { encryptData as encryptString, initJWT as jwt, verifyToken as verifyJwtToken, decryptData as decryptString } from "./utils/jwt-util";
+export { logger } from "./utils/request-util";
