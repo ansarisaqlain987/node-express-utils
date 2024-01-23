c64f807a61cc74b2ace0e9a16e6192d2edd38911
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-01-23 12:50:46 +0530

    added keywords and build commands

--- src/utils/request-util.ts
+++ src/utils/request-util.ts
@@ -1,9 +1,9 @@
-import { v5 } from 'uuid';
+import { v4 } from 'uuid';
 import { ExpressNextFunction, ExpressRequest, ExpressResponse } from "../types";
 
 export const logger = () => {
     return (request: ExpressRequest, response: ExpressResponse, next: ExpressNextFunction) => {
-        const request_id: string = v5('request_id', 'application/uuid');
+        const request_id: string = v4();
         console.log("Request ID: ", request_id);
         request.requestId = request_id;
         request.getRequestId = function () {
