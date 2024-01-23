7ec7fbe752a8fa3f6c7cdc7e35c0b7d6a8409428
Saqlain Ansari <ansarisaqlain987@gmail.com>
2024-01-23 20:35:41 +0530

    added logger functionality

--- src/types.ts
+++ src/types.ts
@@ -1,15 +1,21 @@
 import { NextFunction, Request, Response } from "express";
+import { Logger } from "pino";
 
 export type JWTOptions = {
     secret: string;
     cipherSecret: string;
     headerName?: string;
 }
+export interface ILogger {
+    info: (...args: any[]) => void;
+    error: (...args: any[]) => void;
+}
 
 export type ExpressRequest = Request
     & {
         requestId?: string,
         jwt?: JWTOptions & { user?: string },
+        logger?: Logger,
         getRequestId: () => string,
         getTokenData: () => string | undefined | null,
         createToken: (data: any) => string | null,
--- src/utils/request-util.ts
+++ src/utils/request-util.ts
@@ -1,10 +1,13 @@
 import { v4 } from 'uuid';
 import { ExpressNextFunction, ExpressRequest, ExpressResponse } from "../types";
+import pino from 'pino';
 
 export const logger = () => {
     return (request: ExpressRequest, response: ExpressResponse, next: ExpressNextFunction) => {
         const request_id: string = v4();
-        console.log("Request ID: ", request_id);
+        const logger = pino({})
+        request.logger = logger;
+        logger.info("Request ID: " + request_id);
         request.requestId = request_id;
         request.getRequestId = function () {
             return this.requestId ?? "";
